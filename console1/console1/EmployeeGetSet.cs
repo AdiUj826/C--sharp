a56803488104884763b28cb66823ccf5583be6e5
aditya <adiujgare88@gmail.com>
2026-01-28 22:03:16 +0530

    Add files via upload

---
+++ console1/console1/EmployeeGetSet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace console1
+{
+    internal class EmployeeGetSet
+    {
+        private decimal salary;
+
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public EmployeeGetSet()
+        {
+            EmpId = 1;
+            EmpName = "Rohit";
+            Salary = 25000;
+        }
+
+        public EmployeeGetSet(int id, string name, decimal salary)
+        {
+            EmpId = id;
+            EmpName = name;
+            Salary = salary;
+        }
+
+        public void DisplayEmployee()
+        {
+            Console.WriteLine(EmpId);
+            Console.WriteLine(EmpName);
+            Console.WriteLine(Salary);
+        }
+
+        public bool IsPermanent()
+        {
+            return Salary >= 20000;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee[Id:{EmpId}, Name:{EmpName}, Salary:{Salary}]";
+        }
+    }
+}
---
+++ console1/console1/calculation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace console1
+{
+    internal class program
+    {
+
+        int a = 10, b = 20, c;
+        internal void add()
+        {
+            c = a + b;
+            Console.WriteLine(c);
+        }
+        internal void sub()
+        {
+            c = a - b;
+            Console.WriteLine(c);
+        }
+        internal void mul()
+        {
+            c = a * b;
+            Console.WriteLine(c);
+        }
+        internal void div()
+        {
+            c = a / b;
+            Console.WriteLine(c);
+        }
+
+    }
+}
