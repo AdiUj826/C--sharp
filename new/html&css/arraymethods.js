0cf2cfd29284f6e8693366460c236b6417535046
aditya <adiujgare88@gmail.com>
2026-01-30 20:07:40 +0530

    Add files via upload

---
+++ new/html&css/arraymethods.js
@@ -0,0 +1,83 @@
+//let str =["ram","sham", "laksh" ]
+// // console.log(str.length)
+// // console.log(str.toLowerCase())
+// // console.log(str.toUpperCase())
+// // console.log(str.split('i'))
+// // console.log(str.trim())
+// // console.log(str.trimStart())
+// // console.log(str.trimEnd())
+// //  console.log(str.includes('hi'))
+// console.log(str)
+// // console.log(str.charAt(1))
+// // console.log(str.indexOf('s'))
+// // console.log(str.search("_"))
+// // console.log(str.replace("s", '1'))
+// // console.log(str.replaceAll("i","w"))
+// console.log(str.repeat(2))
+// console.log(str)
+///////////////////task////////////////
+
+/////// Total Number of Students
+// let students = ["Aman", "Ritesh", "Parvez", "Sohel"]
+// console.log(students.length)
+
+// //////// Add Elements/////
+// students.push("Amaan")
+// students.unshift("Sohel")
+// console.log(students)
+
+// /////// Remove Elements////////
+// students.pop()
+// students.shift()
+// console.log(students)
+// /////////Check Element Exists/////
+// console.log(students.includes("Ritesh"))
+
+// //////////// Merge Two Arrays//////////
+// let city1 = ["Delhi", "Mumbai"]
+// let city2 = ["Pune", "Chennai"]
+
+// let allCities = [...city1, ...city2]
+// console.log(allCities)
+
+// //////Convert Array to String//////
+// let colors = ["Red", "Green", "Blue"]
+// console.log(colors.join(", "))
+
+// ///////Find Index///////
+// console.log(students.indexOf("Parvez"))
+
+// ////Slice Students///////
+// let firstThree = students.slice(0, 3)
+// console.log(firstThree)
+
+// ///////Sort Names/////
+// students.sort()
+// console.log(students)
+
+// ////// Find Length of String//////
+// let str = "JavaScript Learning"
+// console.log(str.length)
+
+// /////Convert Case////
+// let name = "Sohel"
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// ///////Get First and Last Character////
+// let word = "JavaScript"
+// console.log(word.charAt(0))
+// console.log(word.charAt(word.length - 1))
+
+// ////// Check Word Exists/////
+// let sentence = "I am learning Angular framework"
+// console.log(sentence.includes("Angular"))
+
+// ///////Replace Word/////
+// let text = "Java is powerful"
+// console.log(text.replace("Java", "JavaScript"))
+
+// //////Remove Extra Spaces//////
+// let msg = "   Hello World   "
+// console.log(msg.trim())
+
---
+++ new/html&css/script.js
@@ -0,0 +1,5 @@
+        //  alert('hii i am message')
+        //  let primitiveValue =12
+        //  console.log(primitiveValue)let msg = "JavaScript chal rahi hai";
+console.log(msg);
+alert(msg);
