a92ebfc6fc37935b237d0d5f2f118aeefcc9b809
aditya <adiujgare88@gmail.com>
2026-01-31 08:36:46 +0530

    Update College.cs

--- OopsGetSet/College.cs
+++ OopsGetSet/College.cs
@@ -6,6 +6,20 @@ class College
     private string location;
     private int collegeCode;
 
+    public College()
+    {
+        name = "NA";
+        location = "NA";
+        collegeCode = 0;
+    }
+
+    public College(string name, string location, int collegeCode)
+    {
+        this.name = name;
+        this.location = location;
+        this.collegeCode = collegeCode;
+    }
+
     public string Name
     {
         get { return name; }
@@ -29,6 +43,7 @@ class College
         Console.WriteLine(Name);
         Console.WriteLine(Location);
         Console.WriteLine(CollegeCode);
+        Console.WriteLine("-----------");
     }
 }
 
@@ -36,10 +51,10 @@ class Program
 {
     static void Main()
     {
-        College c = new College();
-        c.Name = "ABC College";
-        c.Location = "Delhi";
-        c.CollegeCode = 1234;
-        c.PrintInfo();
+        College c1 = new College();
+        c1.PrintInfo();
+
+        College c2 = new College("ABC College", "Delhi", 1234);
+        c2.PrintInfo();
     }
 }
